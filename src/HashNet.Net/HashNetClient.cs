2fa0270b5a6c8723fdab0ed733aeb3bd741a158f
Ervin <ervin@enteralpha.com>
2020-02-26 22:07:42 +0100

    Blockchain apis implemented

--- src/HashNet.Net/HashNetClient.cs
+++ src/HashNet.Net/HashNetClient.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf;
+using Grpc.Core;
 using Grpc.Net.Client;
 using System;
 using System.Collections.Generic;
@@ -15,30 +16,134 @@ namespace HashNet.Net
 
         public HashNetClient(string blockchainEndpoint = "https://localhost:9100", string accountEndpoint = "https://localhost:9100")
         {
-            AccountEndpoint = accountEndpoint;
             BlockchainEndpoint = blockchainEndpoint;
+            AccountEndpoint = accountEndpoint;
         }
 
-        public string AccountEndpoint{ get; set; }
         public string BlockchainEndpoint { get; set; }
+        public string AccountEndpoint{ get; set; }
 
-        public async Task<object> GetAddresses()
-        {
-            var reply = await AccountService.ListAddressesAsync(new ListAddressesRequest());
-            return reply;
-        }
-
-        public async Task<object> GetBlockCount()
+        #region Blockchain
+        /// <summary>
+        /// Gets number of confirmed blocks in current node block chain.
+        /// </summary>
+        /// <returns>GetBlockCountResponse</returns>
+        public async Task<GetBlockCountResponse> GetBlockCount()
         {
             var reply = await BlockchainService.GetBlockCountAsync(new GetBlockCountRequest());
             return reply;
         }
-
-        public async Task<object> GetBlockchainInfo()
+        /// <summary>
+        /// Retrieves confirmed block information from current node block chain.
+        /// </summary>
+        /// <param name="blockHash">Hash for requested block.</param>
+        /// <returns>GetBlockResponse</returns>
+        public async Task<GetBlockResponse> GetBlockByHash(ByteString blockHash)
+        {
+            // var iblockHash = ByteString.CopyFromUtf8(blockHash);
+            var reply = await BlockchainService.GetBlockByHashAsync(new GetBlockByHashRequest() {BlockHash = blockHash });
+            return reply;
+        }
+        /// <summary>
+        /// Retrieves confirmed block information from current node block chain.
+        /// </summary>
+        /// <param name="blockIndex">Block index for requested block.</param>
+        /// <returns>GetBlockResponse</returns>
+        public async Task<GetBlockResponse> GetBlockByIndex(ulong blockIndex)
+        {
+            var reply = await BlockchainService.GetBlockByIndexAsync(new GetBlockByIndexRequest() { BlockIndex = blockIndex });
+            return reply;
+        }
+        /// <summary>
+        /// Retrieves confirmed transaction information from current node block chain.
+        /// </summary>
+        /// <param name="transactionHash">Hash for requested transaction.</param>
+        /// <returns>GetTransactionResponse</returns>
+        public async Task<GetTransactionResponse> GetTransaction(ByteString transactionHash)
+        {
+            var reply = await BlockchainService.GetTransactionAsync(new GetTransactionRequest() { TransactionHash = transactionHash });
+            return reply;
+        }
+        /// <summary>
+        /// Retrieves block chain statistics information for current node block chain.
+        /// </summary>
+        /// <returns>GetBlockchainInfoResponse</returns>
+        public async Task<GetBlockchainInfoResponse> GetBlockchainInfo()
         {
             var reply = await BlockchainService.GetBlockchainInfoAsync(new GetBlockchainInfoRequest());
             return reply;
         }
+        /// <summary>
+        /// Retrieves most recent transaction list based on transaction limit and how many transactions to skip (provides ability to get transactions in batches).
+        /// </summary>
+        /// <param name="addresses">List of all addresses by which transaction should be filtered (leave empty to apply no filter and return all transactions).</param>
+        /// <param name="limit">Maximum number of transactions to return in one batch (no more than 1000).</param>
+        /// <param name="skip">Number of most recent transactions to skip starting from blockchain’s last confirmed block.</param>
+        /// <returns>GetTransactionListResponse</returns>
+        public async Task<GetTransactionListResponse> GetTransactionList(IList<ByteString> addresses, ulong limit, ulong skip)
+        {
+            var req = new GetTransactionListRequest() { Limit = limit, Skip = skip };
+            req.Addresses.AddRange(addresses);
+            var reply = await BlockchainService.GetTransactionListAsync(req);
+            return reply;
+        }
+        /// <summary>
+        /// Get current balance (of tolars) for selected address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <param name="blockIndex">Balance at specific block index.</param>
+        /// <returns>GetBalanceResponse</returns>
+        public async Task<GetBalanceResponse> GetBalance(ByteString address, ulong blockIndex)
+        {
+            var reply = await BlockchainService.GetBalanceAsync(new GetBalanceRequest() { Address = address, BlockIndex = blockIndex });
+            return reply;
+        }
+        /// <summary>
+        /// Get current balance (of tolars) for selected address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <param name="blockIndex"></param>
+        /// <returns>GetBalanceResponse</returns>
+        public async Task<GetBalanceResponse> GetLatestBalance(ByteString address, ulong blockIndex)
+        {
+            var reply = await BlockchainService.GetLatestBalanceAsync(new GetBalanceRequest() { Address = address, BlockIndex = blockIndex });
+            return reply;
+        }
+        /// <summary>
+        /// Get next available nonce value for specific address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <returns>GetNonceResponse</returns>
+        public async Task<GetNonceResponse> GetNonce(ByteString address)
+        {
+            var reply = await BlockchainService.GetNonceAsync(new GetNonceRequest() { Address = address });
+            return reply;
+        }
+        /// <summary>
+        /// Executes read only contract functions on evm without spending gas or having any effect to address balances.
+        /// </summary>
+        /// <param name="senderAddress">Address in Tolar format from which transaction will be send.</param>
+        /// <param name="receiverAddress">Contract address in Tolar format.</param>
+        /// <param name="value">Amount of tolars if needed in contract function.</param>
+        /// <param name="gas">Maximum gas (gas limit) that is available for call function.</param>
+        /// <param name="gasPrice">Amount of gas to pay for each unit of gas, greater gas price is related to faster time to execute transaction (transaction fee = gas * gas price).</param>
+        /// <param name="data">Contract function bytecode in hex format.</param>
+        /// <param name="nonce">Unique transaction index for this sender address (auto-incremented value, each transaction has unique nonce).</param>
+        /// <returns></returns>
+        public async Task<TryCallTransactionResponse> TryCallTransaction(ByteString senderAddress, ByteString receiverAddress, ByteString value, ByteString gas, ByteString gasPrice, string data, ByteString nonce)
+        {
+            var reply = await BlockchainService.TryCallTransactionAsync(new Transaction() { SenderAddress = senderAddress, ReceiverAddress = receiverAddress, Value = value, Gas = gas, GasPrice = gasPrice, Data = data, Nonce = nonce });
+            return reply;
+        }
+        #endregion
+
+        #region Account
+        public async Task<ListAddressesResponse> GetAddresses()
+        {
+            var reply = await AccountService.ListAddressesAsync(new ListAddressesRequest());
+            return reply;
+        }
+        #endregion
 
         private AccountService.AccountServiceClient AccountService
         {
@@ -85,5 +190,7 @@ namespace HashNet.Net
                 return _blockchainService;
             }
         }
+
+
     }
 }
--- src/HashNet.Net/IHashNetClient.cs
+++ src/HashNet.Net/IHashNetClient.cs
@@ -1,14 +1,92 @@
-﻿using System.Threading.Tasks;
+﻿using Google.Protobuf;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tolar.Proto;
 
 namespace HashNet.Net
 {
     public interface IHashNetClient
     {
         string AccountEndpoint { get; set; }
-// IServiceClientAsync HashNetRpcClient { get; }
-        //Task<ListAddressesResponse> GetAddresses();
-        Task<object> GetAddresses();
-        Task<object> GetBlockCount();
-        Task<object> GetBlockchainInfo();
+
+        #region Blockchain
+        /// <summary>
+        /// Gets number of confirmed blocks in current node block chain.
+        /// </summary>
+        /// <returns>GetBlockCountResponse</returns>
+        Task<GetBlockCountResponse> GetBlockCount();
+        /// <summary>
+        /// Retrieves confirmed block information from current node block chain.
+        /// </summary>
+        /// <param name="blockHash">Hash for requested block.</param>
+        /// <returns>GetBlockResponse</returns>
+        Task<GetBlockResponse> GetBlockByHash(ByteString blockHash);
+        /// <summary>
+        /// Retrieves confirmed block information from current node block chain.
+        /// </summary>
+        /// <param name="blockIndex">Block index for requested block.</param>
+        /// <returns>GetBlockResponse</returns>
+        Task<GetBlockResponse> GetBlockByIndex(ulong blockIndex);
+        /// <summary>
+        /// Retrieves confirmed transaction information from current node block chain.
+        /// </summary>
+        /// <param name="transactionHash">Hash for requested transaction.</param>
+        /// <returns>GetTransactionResponse</returns>
+        Task<GetTransactionResponse> GetTransaction(ByteString transactionHash);
+        /// <summary>
+        /// Retrieves block chain statistics information for current node block chain.
+        /// </summary>
+        /// <returns>GetBlockchainInfoResponse</returns>
+        Task<GetBlockchainInfoResponse> GetBlockchainInfo();
+
+        /// <summary>
+        /// Retrieves most recent transaction list based on transaction limit and how many transactions to skip (provides ability to get transactions in batches).
+        /// </summary>
+        /// <param name="addresses">List of all addresses by which transaction should be filtered (leave empty to apply no filter and return all transactions).</param>
+        /// <param name="limit">Maximum number of transactions to return in one batch (no more than 1000).</param>
+        /// <param name="skip">Number of most recent transactions to skip starting from blockchain’s last confirmed block.</param>
+        /// <returns>GetTransactionListResponse</returns>
+        Task<GetTransactionListResponse> GetTransactionList(IList<ByteString> addresses, ulong limit, ulong skip);
+        /// <summary>
+        /// Get current balance (of tolars) for selected address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <param name="blockIndex">Balance at specific block index.</param>
+        /// <returns>GetBalanceResponse</returns>
+        Task<GetBalanceResponse> GetBalance(ByteString address, ulong blockIndex);
+        /// <summary>
+        /// Get current balance (of tolars) for selected address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <param name="blockIndex"></param>
+        /// <returns>GetBalanceResponse</returns>
+        Task<GetBalanceResponse> GetLatestBalance(ByteString address, ulong blockIndex);
+        /// <summary>
+        /// Get next available nonce value for specific address.
+        /// </summary>
+        /// <param name="address">Selected address in Tolar address format.</param>
+        /// <returns>GetNonceResponse</returns>
+        Task<GetNonceResponse> GetNonce(ByteString address);
+        /// <summary>
+        /// Executes read only contract functions on evm without spending gas or having any effect to address balances.
+        /// </summary>
+        /// <param name="senderAddress">Address in Tolar format from which transaction will be send.</param>
+        /// <param name="receiverAddress">Contract address in Tolar format.</param>
+        /// <param name="value">Amount of tolars if needed in contract function.</param>
+        /// <param name="gas">Maximum gas (gas limit) that is available for call function.</param>
+        /// <param name="gasPrice">Amount of gas to pay for each unit of gas, greater gas price is related to faster time to execute transaction (transaction fee = gas * gas price).</param>
+        /// <param name="data">Contract function bytecode in hex format.</param>
+        /// <param name="nonce">Unique transaction index for this sender address (auto-incremented value, each transaction has unique nonce).</param>
+        /// <returns></returns>
+        Task<TryCallTransactionResponse> TryCallTransaction(ByteString senderAddress, ByteString receiverAddress, ByteString value, ByteString gas, ByteString gasPrice, string data, ByteString nonce);
+        #endregion
+
+        #region Account
+        /// <summary>
+        /// Extracting address information from blockchain.
+        /// </summary>
+        /// <returns>ListAddressesResponse</returns>
+        Task<ListAddressesResponse> GetAddresses();
+        #endregion
     }
 }
